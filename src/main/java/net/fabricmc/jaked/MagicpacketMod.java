1609c1d47d728eb50a27e3522be212c4b9123949
Jake Durham <jakedur@gmail.com>
2022-02-15 14:44:04 -0700

    Added support for config file

--- src/main/java/net/fabricmc/jaked/MagicpacketMod.java
+++ src/main/java/net/fabricmc/jaked/MagicpacketMod.java
@@ -1,22 +1,13 @@
 package net.fabricmc.jaked;
 
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.requests.RestAction;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.client.render.DimensionEffects;
 import net.querz.nbt.io.NBTUtil;
-import net.querz.nbt.io.NamedTag;
-import net.querz.nbt.io.SNBTUtil;
 import net.querz.nbt.tag.*;
-import org.apache.commons.lang3.concurrent.Computable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -27,72 +18,97 @@ public class MagicpacketMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
+	// Load config 'magicpacket.properties', if it isn't present create one
+	SimpleConfig CONFIG = SimpleConfig.of( "magicpacket" ).provider( this::provider ).request();
+
+	// Custom config provider, returns the default config content
+	// if the custom provider is not specified SimpleConfig will create an empty file instead
+	private String provider( String filename ) {
+		return "MAC-Address=\nBroadcast-IP=255.255.255.255\nDiscord-Token=\nDiscord-Server-ID=\nDiscord-Channel-ID=\nDiscord-Wait-TimeMS=10000";
+	}
+
+	public final String MAC = CONFIG.getOrDefault( "MAC-Address", "" );
+	public final String BROADCAST = CONFIG.getOrDefault( "Broadcast-IP", "255.255.255.255" );
+	public final String DToken = CONFIG.getOrDefault( "Discord-Token", "" );
+	public final String DServerID = CONFIG.getOrDefault( "Discord-Server-ID", "" );
+	public final String DChannelID = CONFIG.getOrDefault( "Discord-Channel-ID", "" );
+	public final int DWait = CONFIG.getOrDefault( "Discord-Wait-TimeMS", 10000);
+
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Starting magic packet sender");
 
-		String macStr = "08:60:6E:46:EA:FD";
-		byte[] macBytes = new byte[6]; // convert mac to bytes
-		String[] hex = macStr.split("(\\:|\\-)");
-		try {
+		if(!MAC.equals("")) {
+			byte[] macBytes = new byte[6]; // convert mac to bytes
+			String[] hex = MAC.split("(\\:|\\-)");
+			try {
+				for (int i = 0; i < 6; i++) {
+					macBytes[i] = (byte) Integer.parseInt(hex[i], 16);
+				}
+			} catch (Exception e) { LOGGER.error(e.getMessage()); }
+
+			byte[] bytes = new byte[6 + 16 * macBytes.length];
 			for (int i = 0; i < 6; i++) {
-				macBytes[i] = (byte) Integer.parseInt(hex[i], 16);
+				bytes[i] = (byte) 0xff; //pad first 6 bytes
 			}
-		} catch (Exception e) {}
+			for (int i = 6; i < bytes.length; i += macBytes.length) {
+				System.arraycopy(macBytes, 0, bytes, i, macBytes.length); // the rest is the mac bytes repeated 16 times
+			}
 
-		byte[] bytes = new byte[6 + 16 * macBytes.length];
-		for (int i = 0; i < 6; i++) {
-			bytes[i] = (byte) 0xff; //pad first 6 bytes
+			//bytes are ready to be sent (magic packet)
+
+			try {
+
+				InetAddress address = InetAddress.getByName(BROADCAST); // local broadcast ip
+				DatagramPacket packet = new DatagramPacket(bytes, bytes.length, address, 2100);
+				DatagramSocket socket = new DatagramSocket(2100);
+				socket.setBroadcast(true); // make sure to broadcast
+				socket.send(packet);
+				socket.close();
+				LOGGER.info("Wake up sent");
+
+			} catch (Exception e) {
+				LOGGER.error(e.getMessage());
+			}
 		}
-		for (int i = 6; i < bytes.length; i += macBytes.length) {
-			System.arraycopy(macBytes, 0, bytes, i, macBytes.length); // the rest is the mac bytes repeated 16 times
+		else {
+			LOGGER.info("No MAC provided, so no magic packet sent");
 		}
 
-		//bytes are ready to be sent (magic packet)
+		if(!DToken.equals("") && !DChannelID.equals("") && !DServerID.equals(""))
+			try { // update server ip on startup
+				JDA bot = JDABuilder.createDefault(DToken).build();
+				bot.awaitReady();
 
-		try {
+				synchronized (bot) {
+					bot.wait(DWait);
+				}
 
-			InetAddress address = InetAddress.getByName("255.255.255.255"); // local broadcast ip
-			DatagramPacket packet = new DatagramPacket(bytes, bytes.length, address, 2100);
-			DatagramSocket socket = new DatagramSocket(2100);
-			socket.setBroadcast(true); // make sure to broadcast
-			socket.send(packet);
-			socket.close();
-			LOGGER.info("Wake up sent");
+				//get most recent ip
+				bot.getGuildById(DServerID).getTextChannelById(DChannelID).getHistory().retrievePast(1).map(messages -> messages.get(0)).queue(message -> {
 
-		} catch (Exception e) {LOGGER.error(e.getMessage());}
+					// construct server file
+					ListTag lt = new ListTag(CompoundTag.class);
+					CompoundTag innerCt = new CompoundTag();
 
-		try { // update server ip on startup
-			JDA bot = JDABuilder.createDefault("OTQyMzI3NDU0NzUwNjA5NDA5.Ygi41A.5Sav4_M9pazxBUnYbIh6EK0Cun0").build();
-			bot.awaitReady();
+					innerCt.putString("name", "Minecraft Server");
+					innerCt.putString("icon", "");
+					innerCt.putString("ip", message.getContentDisplay());
+					lt.add(innerCt);
 
-			String ip;
-			//get most recent ip
-			bot.getGuildById("941402252810260522").getTextChannelById("941402253254877217").getHistory().retrievePast(1).map(messages -> messages.get(0)).queue(message -> {
+					CompoundTag ct = new CompoundTag();
+					ct.put("servers", lt);
 
-				// construct server file
-				ListTag lt = new ListTag(CompoundTag.class);
-				CompoundTag innerCt = new CompoundTag();
-
-				innerCt.putString("name", "Dorm Server!");
-				innerCt.putString("icon", "");
-				innerCt.putString("ip", message.getContentDisplay());
-				lt.add(innerCt);
-
-				CompoundTag ct = new CompoundTag();
-				ct.put("servers", lt);
-
-				NamedTag nt = new NamedTag("", ct);
-
-
-				try {
-					NBTUtil.write(ct, System.getProperty("user.dir")+"/servers.dat", false);
-					LOGGER.info("Replaced Server");
-				} catch (Exception e) {e.printStackTrace();}
-			});
-			bot.shutdown();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		};
+					try {
+						NBTUtil.write(ct, System.getProperty("user.dir") + "/servers.dat", false);
+						LOGGER.info("Replaced Server");
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				});
+				bot.shutdown();
+			} catch (Exception ex) { ex.printStackTrace(); }
+		else
+			LOGGER.info("Need Tokens/ID for discord");
 	}
 }
